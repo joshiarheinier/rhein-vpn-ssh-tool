c48051398d79e26207ba346628af661d789d1032
Joshia Pardamean <joshia.pardamean@ovo.id>
2019-10-14 16:56:35 +0700

    Modify vpn-stop to be configurable

--- Rhein/vpn-stop.sh
+++ Rhein/vpn-stop.sh
@@ -1,9 +1,13 @@
-if [ $1 == "stg" ];
+#!/bin/bash
+DIRPATH=$2
+CONFIG=`echo $DIRPATH/config/config-$1.cnf`
+CLIENT=`cat $CONFIG | grep client | cut -d'=' -f2`
+if [ $CLIENT == "openvpn" ];
 then
 	PID=`ps aux | grep sbin/openvpn | grep -v grep | awk '{print $2}'`
 	sudo kill -9 $PID
 	echo "VPN successfully disconnected."
-elif [ $1 == "dev" ] || [ $1 == "prod" ];
+elif [ $CLIENT == "anyconnect" ];
 then
 	/opt/cisco/anyconnect/bin/vpn disconnect
 	echo "VPN successfully disconnected."
